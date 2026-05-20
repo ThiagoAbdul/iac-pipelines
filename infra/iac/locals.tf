8531bd1f8b105870066cd1ab6636ab9114b60ea2
ThiagoAbdul <thiagoabdul10@gmail.com>
2026-05-19 23:45:36 -0300

    feat: hello world

---
+++ infra/iac/locals.tf
@@ -0,0 +1,7 @@
+locals {
+  env = {
+    name           = var.env_name
+    aws_region     = var.aws_region
+    aws_account_id = var.aws_account_id
+  }
+}
---
+++ src/ApiDemo.Tests/HelloWorldTests.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiDemo.Tests;
+
+public class HelloWorldTests
+{
+    [Fact]
+    public void Hello()
+    {
+        Assert.Equal("Hello", nameof(HelloWorld.Hello));
+        Assert.Equal("World", HelloWorld.Create().Hello);
+    }
+}
