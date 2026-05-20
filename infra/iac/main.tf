ccdfb23b9f09ba82260a416af484c1df9977399f
ThiagoAbdul <thiagoabdul10@gmail.com>
2026-05-20 00:44:42 -0300

    ci: improve

--- infra/iac/main.tf
+++ infra/iac/main.tf
@@ -1,6 +1,6 @@
 module "image_repository"{
   source = "git::https://github.com/ThiagoAbdul/tf-templates.git//modules/image_repository"
   app_name = var.app_name
-  namespace = "pipelines"
+  namespace = var.namespace
   env = local.env
 }
